0f553656d70884b2524b8a30b9d635cf615742a6
parentClass <dimalantadaniel@gmail.com>
2024-09-21 19:25:32 +0800

    refactor form fields

--- js/script.js
+++ js/script.js
@@ -27,66 +27,26 @@ function uploadImages(e) {
             j = JSON.parse(xhr.response);
 
             // ceremony
-            if(j.ceremony.success.length > 0) {
+            if(j.success.length > 0) {
                 j.ceremony.success.map(element => {
-                    $("#ceremony-success-list").append('<li>' + element + '</li>')
+                    $("#wedding-success-list").append('<li>' + element + '</li>')
                 })
 
-                $("#ceremony-success").slideDown();
+                $("#wedding-success").slideDown();
             }
 
-            if(j.ceremony.failed.length > 0) {
+            if(j.failed.length > 0) {
                 j.ceremony.failed.map(element => {
-                    $("#ceremony-failed-list").append('<li>' + element + '</li>')
+                    $("#wedding-failed-list").append('<li>' + element + '</li>')
                 })
                 
-                $("#ceremony-failed").slideDown();
-            }
-
-            // reception
-            if(j.reception.success.length > 0) {
-                j.reception.success.map(element => {
-                    $("#reception-success-list").append('<li>' + element + '</li>')
-                })
-
-                $("#reception-success").slideDown();
-            }
-
-            if(j.reception.failed.length > 0) {
-                j.reception.failed.map(element => {
-                    $("#reception-failed-list").append('<li>' + element + '</li>')
-                })
-                
-                $("#reception-failed").slideDown();
-            }
-
-            // after party
-            if(j.afterparty.success.length > 0) {
-                j.afterparty.success.map(element => {
-                    $("#afterparty-success-list").append('<li>' + element + '</li>')
-                })
-
-                $("#afterparty-success").slideDown();
-            }
-
-            if(j.afterparty.failed.length > 0) {
-                j.afterparty.failed.map(element => {
-                    $("#afterparty-failed-list").append('<li>' + element + '</li>')
-                })
-                
-                $("#afterparty-failed").slideDown();
+                $("#wedding-failed").slideDown();
             }
 
             setTimeout(function() {
-                $("#ceremony-success").slideUp("slow");
-                $("#reception-success").slideUp("slow");
-                $("#afterparty-success").slideUp("slow");
-                $("#ceremony-failed").slideUp("slow");
-                $("#reception-failed").slideUp("slow");
-                $("#afterparty-failed").slideUp("slow");
-                document.querySelector('#ceremonyFile').value = '';
-                document.querySelector('#receptionFile').value = '';
-                document.querySelector('#afterPartyFile').value = '';
+                $("#wedding-success").slideUp("slow");
+                $("#wedding-failed").slideUp("slow");
+                document.querySelector('#weddingFile').value = '';
             }, 5000);
         } else {
             // Error during upload
